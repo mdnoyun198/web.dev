ae4e4451bad33a3fdcbb485224abca7fe5ca5f12
Md Noyun <mdnoyun198@gmail.com>
2025-09-04 11:37:25 +0600

    Add files via upload

---
+++ JvaScript/@React/@Projict/DataCard/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+
+import React from "react";
+
+
+const Navbar = () => {
+    return (
+
+        <div className="Navbar flex w-[100%] bg-blue-500">
+            <nav className="flex gap-2">
+                <ol>
+                    <li>Home</li>
+                    <li>about</li>
+                    <li>content</li>
+                </ol>
+            </nav>
+
+        </div>
+
+    );
+};
+
+export default Navbar;
