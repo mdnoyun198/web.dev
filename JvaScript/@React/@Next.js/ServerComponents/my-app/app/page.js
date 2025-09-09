3fa5abdb1081b80360755914425fc3fd9a99d99e
Md Noyun <mdnoyun198@gmail.com>
2025-09-09 11:05:06 +0600

    Add files via upload

---
+++ JvaScript/@React/@Next.js/ServerComponents/my-app/app/page.js
@@ -0,0 +1,23 @@
+
+
+"use client"
+
+import Navbar from "@/components/Navbar";
+
+import { useState, useEffect } from "react";
+
+export default function Home() {
+
+  const [count, setcount] = useState(0)
+  return (
+
+    <div>
+      <Navbar />
+    </div>
+
+
+
+
+
+  );
+}
